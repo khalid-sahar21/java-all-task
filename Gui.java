ce146d450c591bb15e58e9763ae7e73d881b1016
khalid sahar <khalidsahar987@gmail.com>
2023-10-10 00:54:29 -0700

    all java class task and code

---
+++ Gui.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Gui
+{
+    public static void main(String[] args) {
+        Frame frm = new Frame("khalid Gui program");
+        Label lbl = new label("well come to khalid ",Label.CENTER);
+        frm.add(lbl);
+        frm.setSize(400,200);
+        frm.setVisible(ture);
+
+    }
+}
---
+++ MyFirstcode.java
@@ -0,0 +1,17 @@
+import java.swing.*;
+public class MyFristcode{
+public static void main(String[] args) {
+JFrame frame=new JFrame();
+frame frame=new frame.getContantpane();
+con.setLayout(new FlowLayout());
+FlowLayout layout=new FlowLayout();
+con.setLayout(layout);
+JButton bt1=new JButton("ok");
+JButton bt2=new JButton("Quit");
+con.add(bt1);
+con.add(bt2);
+frame.setSize(200,200);
+frame.setVisible(true);
+
+}   
+}
